14450647ee7136e738199d36f3c8c8a7101fa8fe
Regina Musovirova <musovirova@yahoo.com>
2020-04-20 14:20:49 -0400

    finish

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: '557597753323',
+    password: '',
     database: 'employeesDB'
 });
 
